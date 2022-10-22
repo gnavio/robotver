7e4df99b772f708c971d9afc379d33c87d0620a2
gnavio <100093310+gnavio@users.noreply.github.com>
2022-10-22 17:01:41 +0200

    Modelado nivel 1

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -6,19 +6,44 @@ public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
     public float gravity = -9.8f;
-    private CharacterController _charController;
+    public float jumpForce;
+    private float ySpeed;
+    private CharacterController _charController; 
+    bool jumped = false;
+
     void Start()
     {
         _charController = GetComponent<CharacterController>();
     }
     void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed; // Las teclas asociadas están en:
+        float deltaX = Input.GetAxis("Horizontal") * speed; // Las teclas asociadas estï¿½n en:
         float deltaZ = Input.GetAxis("Vertical") * speed; // Edit\Project Settings\Input
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
-        movement.y = gravity;
         movement = transform.TransformDirection(movement); // convierte desde el sistema local al global
+
+        if(_charController.isGrounded)
+        {
+            if(Input.GetButtonDown("Jump")) 
+            {
+                jumped = true;
+            }
+        } else { ySpeed += gravity * Time.deltaTime; }
+        
+        movement.y = ySpeed;
         _charController.Move(movement * Time.deltaTime); // no movemos el transform para que se calculen
     }
+
+    private void FixedUpdate()
+    {
+        if(jumped)
+        {
+            jumped = false;
+            ySpeed = gravity * Time.deltaTime;
+            ySpeed = jumpForce;
+        }
+    }
 }
+
+ 
