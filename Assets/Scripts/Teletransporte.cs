24bac4f486036fa91c880dd6bc99f1e509733e8b
gnavio <100093310+gnavio@users.noreply.github.com>
2022-11-19 14:33:07 +0100

    Base teletransporte

---
+++ Assets/Scripts/Teletransporte.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teletransporte : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    [SerializeField] private Camera _camera;
+
+    [SerializeField] Transform orientation;
+    [SerializeField] Animator anim;
+    [SerializeField] public GameObject OverlayPrefab;
+    [SerializeField] public GameObject overlayPos;
+
+    public int cartuchos = 30;
+
+    [Header("Tecla")]
+    [SerializeField] KeyCode DashKey = KeyCode.T; //Añadir sistema para alternar entre Impulso y Teletransporte
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("Impulso", false);
+
+        if (Input.GetKeyDown(DashKey) && cartuchos >= 1) 
+        {   
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                m_Rigidbody.position = hit.point; //Teletransporte
+            }
+            
+            cartuchos -= 1;
+            anim.SetBool("Impulso", true);
+            Debug.Log("ImpulsoTrue");
+            StartCoroutine(ImpulsoOverlay());
+        }
+    }
+
+    public void SumarCartucho(int x)
+    {
+        cartuchos += x;
+    }
+
+    IEnumerator ImpulsoOverlay()
+    {
+        GameObject overlay = Instantiate(OverlayPrefab, overlayPos.transform.position, overlayPos.transform.rotation);
+        overlay.transform.parent = overlayPos.transform;
+        yield return new WaitForSeconds(1);
+        Destroy(overlay);
+    }
+}
